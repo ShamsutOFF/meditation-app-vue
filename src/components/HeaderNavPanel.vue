7397e6c821ad13357e766df47e23dd1e6af325b9
ShamsutOFF <shamsutoff@yandex.ru>
2026-02-11 10:42:40 +0300

    Добавил хедер

---
+++ src/components/HeaderNavPanel.vue
@@ -0,0 +1,46 @@
+<script setup lang="ts">
+import LogoIcon from '@/icons/LogoIcon.vue';
+import NavTab from '@/components/NavTab.vue';
+import StatIcon from '@/icons/StatIcon.vue';
+import RunMeditationIcon from '@/icons/RunMeditationIcon.vue';
+import ExitIcon from '@/icons/ExitIcon.vue';
+
+</script>
+
+<template>
+  <div class="header-container">
+    <LogoIcon />
+    <nav class="nav-panel">
+      <NavTab
+        :icon="RunMeditationIcon"
+        text="Медитация"
+        to="/"
+      />
+      <NavTab
+        :icon="StatIcon"
+        text="Статистика"
+        to="/"
+      />
+      <NavTab
+        :icon="ExitIcon"
+        text="Выход"
+        to="/"
+      />
+    </nav>
+  </div>
+</template>
+
+<style scoped>
+.header-container  {
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  padding: 16px 24px;
+}
+
+.nav-panel {
+  display: flex;
+  gap: 16px;
+  align-items: center;
+}
+</style>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+import HeaderNavPanel from '@/components/HeaderNavPanel.vue';
+import MeditationsCardsList from '@/components/MeditationsCardsList.vue';
+</script>
+
+<template>
+  <div class="page-container">
+    <HeaderNavPanel />
+    <MeditationsCardsList />
+  </div>
+</template>
+
+<style scoped>
+.page-container {
+  display: flex;
+  flex-direction: column;
+  max-width: 1280px;
+  margin: 0 auto;
+}
+</style>
